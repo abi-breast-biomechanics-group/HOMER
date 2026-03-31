7007fa11467fcf874e257c3d3b7aebcad3079b18
Robin Laven <r.laven@auckland.ac.nz>
2026-04-01 10:33:00 +1300

    improved visualisation to focus on diagonal strain vector components

--- tests/test_strain_eval.py
+++ tests/test_strain_eval.py
@@ -4,14 +4,14 @@ import numpy as np
 import pyvista as pv
 
 
-point0 = MeshNode(loc=([0,0,1]))
-point1 = MeshNode(loc=([0,0,0]))
-point2 = MeshNode(loc=([0,1,1]))
-point3 = MeshNode(loc=([0,1,0]))
-point4 = MeshNode(loc=([1,0,1]))
-point5 = MeshNode(loc=([1,0,0]))
-point6 = MeshNode(loc=([1,1,1]))
-point7 = MeshNode(loc=([1,1,0]))
+point0 = MeshNode(loc=([0,0,0]))
+point1 = MeshNode(loc=([1,0,0]))
+point2 = MeshNode(loc=([0,1,0]))
+point3 = MeshNode(loc=([1,1,0]))
+point4 = MeshNode(loc=([0,0,1]))
+point5 = MeshNode(loc=([1,0,1]))
+point6 = MeshNode(loc=([0,1,1]))
+point7 = MeshNode(loc=([1,1,1]))
 element1 = MeshElement(node_indexes=[0,1,2,3,4,5,6,7], basis_functions=(L1Basis, L1Basis, L1Basis))
 mesh0 = Mesh(nodes = [point0, point1, point2, point3, point4, point5, point6, point7], elements = element1).rebase([H3Basis]*3)
 
@@ -21,20 +21,24 @@ elems = np.zeros(grid.shape[0], dtype=int)
 
 wlocs = mesh0.evaluate_embeddings_in_every_element(grid)
 #apply the strain mapping
-wlocs_def = np.stack((wlocs[:, 0], wlocs[:, 1] + wlocs[:, 0] * 0.1, wlocs[:, 2]**2), axis=1)
+wlocs_def = np.stack((wlocs[:, 0], wlocs[:, 1] + wlocs[:, 0] * 0.1, wlocs[:,2] + 0.1 * wlocs[:, 2]**2), axis=1)
 
 #fit the deformed mesh to this strain field (it can represent it perfectly)
 wmat = mesh_def.get_xi_weight_mat(elems, grid)
 mesh_def.linear_fit(wlocs_def, weight_mat=wmat)
 
 # evaluate the strain field over this deformation
+grid = mesh0.xi_grid(res=5)
 strains = mesh0.evaluate_strain_in_every_element(grid, mesh_def)
+slocs = mesh0.evaluate_embeddings(0, grid)
+svecs = mesh0.evaluate_jacobians(0, grid)
+
 
 s = pv.Plotter()
-mesh0.plot(s)
-mesh_def.plot(s, node_colour='g')
+mesh0.plot(s, mesh_opacity=0.1)
+mesh_def.plot(s, node_colour='g', mesh_opacity=0.1)
 s.add_mesh(np.array(wlocs_def), render_points_as_spheres=True)
-s.add_arrows(wlocs, strains[:, 0], color='r')
-s.add_arrows(wlocs, strains[:, 1], color='g')
-s.add_arrows(wlocs, strains[:, 2], color='b')
+s.add_arrows(slocs, strains[:, 0, 0][:, None] * svecs[:, 0], color='r')
+s.add_arrows(slocs, strains[:, 1, 1][:, None] * svecs[:, 1], color='g')
+s.add_arrows(slocs, strains[:, 2, 2][:, None] * svecs[:, 2], color='b')
 s.show()
